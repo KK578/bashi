961c3dd978926d4913a1ce6520196d47d201d181
Kevin Kwan <KevinKwan95@googlemail.com>
2019-12-08 11:59:08 +0000

    Add projects with StyleCop setup

---
+++ src/Bashi.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2019-2019 (c) Bashi. All rights reserved.
+// Licensed under the BSD-3-Clause license. See BSD-3-Clause.md for full details.
+
+namespace Bashi.Console
+{
+    /// <summary>
+    /// Bashi Command Line Interface application entry point.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The application entry point method to run Bashi commands.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
