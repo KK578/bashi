bc57f3452b6fb8db0fd786b0d45daf39ef45f1d0
Kevin Kwan <KevinKwan95@googlemail.com>
2019-12-11 23:23:54 +0000

    Add CommandLineUtils.

---
+++ src/Bashi.Console/Properties/AssemblyProperties.cs
@@ -0,0 +1,6 @@
+// Copyright 2019-2019 (c) Bashi. All rights reserved.
+// Licensed under the BSD-3-Clause license. See BSD-3-Clause.md for full details.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Bashi.Console.Tests")]
